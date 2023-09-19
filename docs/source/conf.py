90e6520fccd0bf5ec6db2c3ebd0fbdfc159abb7e
Espion <its.espion@proton.me>
2023-09-19 15:52:57 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,10 +4,10 @@
 
 project = "Source2Blender"
 copyright = "2023, Espion"
-author = "Espion"
+author = "TF2 Blender Community"
 
-release = "0.1"
-version = "0.1.0"
+release = "1.0"
+version = "1.0.0"
 
 # -- General configuration
 
